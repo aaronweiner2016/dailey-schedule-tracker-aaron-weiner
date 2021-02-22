12bad316a2ad9d1950ee9ce35e6070f737cd2d44
aaron weiner <aaronjacobweiner@yahoo.com>
2021-02-22 18:24:26 -0500

    add some changes

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,6 +3,7 @@ var time = moment().format("kk");
 
 $("#currentDay").text(date);
 
+
 var contentNine = document.getElementById("content-nine");
 var contentTen = document.getElementById("content-ten");
 var contentEleven = document.getElementById("content-eleven");
@@ -13,26 +14,17 @@ var contentThree = document.getElementById("content-three");
 var contentFour = document.getElementById("content-four");
 var contentFive = document.getElementById("content-five");
 
+var saveBtn = document.querySelector(".save");
  
 var timeNine = moment("9","kk").format("kk");
-// $("#time-nine").text(timeNine);
 var timeTen = moment("10","kk").format("kk");
-// $("#time-ten").text(timeTen);
 var timeEleven = moment("11","kk").format("kk");
-// $("#time-eleven").text(timeEleven);
 var timeTwelve = moment("12","kk").format("kk");
-// $("#time-twelve").text(timeTwelve);
 var timeOne = moment("13","kk").format("kk");
-// $("#time-one").text(timeOne);
 var timeTwo = moment("14","kk").format("kk");
-// $("#time-two").text(timeTwo);
 var timeThree = moment("15","kk").format("kk");
-// $("#time-three").text(timeThree);
 var timeFour = moment("16","kk").format("kk");
-// $("#time-four").text(timeFour);
 var timeFive = moment("17","kk").format("kk");
-// $("#time-five").text(timeFive);
-
 
 var arrayTimes = [timeNine, timeTen, timeEleven, timeTwelve, timeOne, timeTwo, timeThree, timeFour, timeFive];
 var contentArray = [contentNine, contentTen, contentEleven, contentTwelve, contentOne, contentTwo, contentThree, contentFour, contentFive]
@@ -40,113 +32,153 @@ var contentArray = [contentNine, contentTen, contentEleven, contentTwelve, conte
 
 for(i = 0; i < arrayTimes.length; i++){
     if(arrayTimes[i] === time){
-        contentArray[i].setAttribute("style", "background-color: red;");
+        contentArray[i].classList.add("present");
     }else if(arrayTimes[i] < time){
-        contentArray[i].setAttribute("style", "background-color: grey;");
+        contentArray[i].classList.add("past");
     }else{
-        contentArray[i].setAttribute("style", "background-color: green;");
+        contentArray[i].classList.add("future");
     }
+    // update textcontent with localstorage value for that  time
+
 }
 
-if (timeTwo < time){
-    console.log("bad");
-}else{
-    console.log("good");
-}
 
-console.log(time);
-console.log(timeOne, timeTwo, timeNine, timeTen, timeEleven, timeTwelve, timeThree, timeFour, timeFive);
+var infoArray = [];
 
+var eventItem = document.querySelector("#event");
+var eventItemTwo = document.querySelector("#event-two");
+var eventItemThree = document.querySelector("#event-three");
+var eventItemFour = document.querySelector("#event-four");
+var eventItemFive = document.querySelector("#event-five");
+var eventItemSix = document.querySelector("#event-six");
+var eventItemSeven = document.querySelector("#event-seven");
+var eventItemEight = document.querySelector("#event-eight");
+var eventItemNine = document.querySelector("#event-nine");
 
+// var events = [eventItem, eventItemTwo, eventItemThree, eventItemFour, eventItemFive, eventItemSix, eventItemSeven, eventItemEight, eventItemNine];
 
+var saveBtn = document.querySelector("#save-one");
+var saveBtnTwo = document.querySelector("#save-two");
+var saveBtnThree = document.querySelector("#save-three");
+var saveBtnFour = document.querySelector("#save-four");
+var saveBtnFive = document.querySelector("#save-five");
+var saveBtnSix = document.querySelector("#save-six");
+var saveBtnSeven = document.querySelector("#save-seven");
+var saveBtnEight = document.querySelector("#save-eight");
+var saveBtnNine = document.querySelector("#save-nine");
 
-
-// if(timeOne > timeTwelve){
-//     console.log("good");
-// }else{
-//     console.log("bad");
-// }
-
-// if(timeEleven <= moment()){
-//     console.log("good");
-// }else{
-//     console.log("bad");
-// }
-
-// if (timeNine === time){
-//     contentNine.setAttribute("style", "background-color: red;")
-// }else if (timeNine < time){ // timenine < timeten???????
-//     contentNine.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentNine.setAttribute("style", "background-color: green;")
-// }//????????????????????
-
-// if (timeTen === time){
-//     contentTen.setAttribute("style", "background-color: red;")
-// }else if (timeTen < time){ //timeten < timeeleven??????/
-//     contentTen.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentTen.setAttribute("style", "background-color: green;")
-// }//??????????????????
-
-// if (timeEleven === time){
-//     contentEleven.setAttribute("style", "background-color: red;")
-// }else if (timeEleven < time){
-//     contentEleven.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentEleven.setAttribute("style", "background-color: green;")
-// }//???????????????
-
-// if (timeTwelve === time){
-//     contentTwelve.setAttribute("style", "background-color: red;")
-// }else if (timeTwelve < time){
-//     contentTwelve.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentTwelve.setAttribute("style", "background-color: green;")
-// }//?????????????????????
-
-// if (timeOne === time){
-//     contentOne.setAttribute("style", "background-color: red;")
-// }else if (timeOne < time){
-//     contentOne.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentOne.setAttribute("style", "background-color: green;")
-// }
-
-// if (timeTwo === time){
-//     contentTwo.setAttribute("style", "background-color: red;")
-// }else if (timeTwo < time){
-//     contentTwo.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentTwo.setAttribute("style", "background-color: green;")
-// }
-
-// if (timeThree === time){
-//     contentThree.setAttribute("style", "background-color: red;")
-// }else if (timeThree < time){
-//     contentThree.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentThree.setAttribute("style", "background-color: green;")
-// }
-
-// if (timeFour === time){
-//     contentFour.setAttribute("style", "background-color: red;")
-// }else if (timeFour < time){
-//     contentFour.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentFour.setAttribute("style", "background-color: green;")
-// }
-
-// if (timeFive === time){
-//     contentFive.setAttribute("style", "background-color: red;")
-// }else if (timeFive < time){
-//     contentFive.setAttribute("style", "background-color: grey;")
-// }else{
-//     contentFive.setAttribute("style", "background-color: green;")
-// }
+var p = document.querySelector("#p");
+// var save = [saveBtn, saveBtnTwo, saveBtnThree, saveBtnFour, saveBtnFive, saveBtnSix, saveBtnSeven, saveBtnEight, saveBtnNine];
+    
 
 
-// function submit(){
+// var keys = localStorage.setItem([("messages",key),("messages-two", keyTwo),( keyThree), keyfour, keyfive, keySix, keySeven, keyEight, keyNine]);
+
+
+
+// for(i = 0; i < events.length; i++){
 
 // }
 
+// for(i = 0; i < save.length; i++){
+
+// }
+
+// for(i = 0; i < keys.length; i++){
+
+// }
+
+
+// function init() {
+//     renderMessage()
+// }
+
+
+
+// saveBtn.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var key = eventItem.value;
+//     localStorage.setItem("messages", key);
+//     renderMessage();
+    
+    
+// });
+// function renderMessage() {
+//     var messagesBack = localStorage.getItem("messages");
+    
+//     eventItem.value = messagesBack;
+// }
+
+
+$(".save").on("click", function() {
+    console.logv("hit")
+    console.log($(this));
+    console.log($(this).siblings());
+
+    // var timeName = $(this).siblings(".event").attr("id");
+    // var message = $(this).siblings(".event").val();
+
+    // localStorage.setItem(timeName, message);
+})
+
+
+// saveBtnTwo.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyTwo = eventItemTwo.value;
+//     localStorage.setItem("messages-two", keyTwo);
+   
+// });
+
+// saveBtnThree.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyThree = eventItemThree.value;
+//     localStorage.setItem("messages-three", keyThree);
+    
+// });
+
+// saveBtnFour.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyfour = eventItemFour.value;
+//     localStorage.setItem("messages-four", keyfour);
+   
+// });
+
+// saveBtnFive.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyfive = eventItemFive.value;
+//     localStorage.setItem("messages-five", keyfive);
+   
+// });
+
+// saveBtnSix.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keySix = eventItemSix.value;
+//     localStorage.setItem("messages-six", keySix);
+   
+// });
+
+// saveBtnSeven.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keySeven = eventItemSeven.value;
+//     localStorage.setItem("messages-seven", keySeven);
+    
+// });
+
+// saveBtnEight.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyEight = eventItemEight.value;
+//     localStorage.setItem("messages-eight", keyEight);
+   
+// });
+
+// saveBtnNine.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var keyNine = eventItemNine.value;
+//     localStorage.setItem("messages-nine", keyNine);
+    
+// });
+
+
+
+
+// init();
