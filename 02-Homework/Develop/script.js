9057ef166b325747fe55e5b9f858381aa564d3d1
aaron weiner <aaronjacobweiner@yahoo.com>
2021-02-24 17:23:34 -0500

    consolidating and adding comments, plus adding my readme

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,9 +1,16 @@
-var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-var time = moment().format("kk");
+var time = moment().format("kk");          // my invisible time to compare for my background color changes 
 
-$("#currentDay").text(date);
+$("#currentDay").html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));        //current date called to show immediately on page
+
+function update() {
+    $("#currentDay").html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));        // date called again to create a function
+}
+
+setInterval(update,1000);       // calling my date function to update every second
 
 time = "09";
+        // getting my textareas from html
+
 var contentNine = document.getElementById("hour-9");
 var contentTen = document.getElementById("hour-10");
 var contentEleven = document.getElementById("hour-11");
@@ -14,8 +21,12 @@ var contentThree = document.getElementById("hour-3");
 var contentFour = document.getElementById("hour-4");
 var contentFive = document.getElementById("hour-5");
 
+        // getting my save buttons from html
+
 var saveBtn = document.querySelector(".save");
  
+        // creating variables for the times
+
 var timeNine = moment("9","kk").format("kk");
 var timeTen = moment("10","kk").format("kk");
 var timeEleven = moment("11","kk").format("kk");
@@ -26,11 +37,13 @@ var timeThree = moment("15","kk").format("kk");
 var timeFour = moment("16","kk").format("kk");
 var timeFive = moment("17","kk").format("kk");
 
-
+        // creating arrays for my for loop to sort through
 
 var arrayTimes = [timeNine, timeTen, timeEleven, timeTwelve, timeOne, timeTwo, timeThree, timeFour, timeFive];
 var contentArray = [contentNine, contentTen, contentEleven, contentTwelve, contentOne, contentTwo, contentThree, contentFour, contentFive]
 
+
+        // my for loop to go through my contentArray and arrayTimes which correlate eachother and add the classes as needed
 
 for(i = 0; i < arrayTimes.length; i++){
     if(arrayTimes[i] === time){
@@ -40,44 +53,20 @@ for(i = 0; i < arrayTimes.length; i++){
     }else{
         contentArray[i].classList.add("future");
     }
-
 }
 
-
-// var infoArray = [];
-
-// var eventItem = document.querySelector("#event");
-// var eventItemTwo = document.querySelector("#event-two");
-// var eventItemThree = document.querySelector("#event-three");
-// var eventItemFour = document.querySelector("#event-four");
-// var eventItemFive = document.querySelector("#event-five");
-// var eventItemSix = document.querySelector("#event-six");
-// var eventItemSeven = document.querySelector("#event-seven");
-// var eventItemEight = document.querySelector("#event-eight");
-// var eventItemNine = document.querySelector("#event-nine");
-
-
-// var saveBtn = document.querySelector("#save-one");
-// var saveBtnTwo = document.querySelector("#save-two");
-// var saveBtnThree = document.querySelector("#save-three");
-// var saveBtnFour = document.querySelector("#save-four");
-// var saveBtnFive = document.querySelector("#save-five");
-// var saveBtnSix = document.querySelector("#save-six");
-// var saveBtnSeven = document.querySelector("#save-seven");
-// var saveBtnEight = document.querySelector("#save-eight");
-// var saveBtnNine = document.querySelector("#save-nine");
-
-// var p = document.querySelector("#p");
-
-
+        // creating a button click which will save the box of text to the local storage
 
 $(".save").on("click", function(event) {
    event.preventDefault();
     var timeName = $(this).siblings(".event").attr("id");
     var message = $(this).siblings(".event").val();
     localStorage.setItem(timeName, message);
+    console.log(timeName);
+    console.log(message);
 })
 
+        // getting my items out of local storage to have on page
 
 $("#hour-9").val(localStorage.getItem("hour-9"));
 $("#hour-10").val(localStorage.getItem("hour-10"));
@@ -88,4 +77,3 @@ $("#hour-2").val(localStorage.getItem("hour-2"));
 $("#hour-3").val(localStorage.getItem("hour-3"));
 $("#hour-4").val(localStorage.getItem("hour-4"));
 $("#hour-5").val(localStorage.getItem("hour-5"));
-
--- 02-Homework/README.md
+++ 02-Homework/README.md
@@ -1,1 +1,4 @@
 # dailey-schedule-tracker-aaron-weiner
+
+### Abou this Page
+<p>For this website i created a dailey planner that allows the user to see a live time and also see what things are on the schedule for the day. This project helped me enforce my skills in writing funtions further but it mainly helped in seeing how jquery can consolidate and make for a cleaner looking code. 
